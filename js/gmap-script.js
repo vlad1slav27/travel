b77f0810318ae7c8dbe98b1dc93015823370cd95
Vlad <kalatun4ik@gmail.com>
2019-01-28 22:13:42 +0200

    final

---
+++ js/gmap-script.js
@@ -0,0 +1,191 @@
+function initMap() {
+  var map, marker,
+    mapDiv = document.getElementById('map'),
+    center = {
+      lat: 41.920187,
+      lng: 12.497144
+  }
+  var markers = [{
+    position : center,
+    icon : 'img/marker.png'
+    },{
+      position : { lat: 46.052630, lng: 4.845418},
+      icon : 'img/marker.png'
+    },{
+      position : { lat: 40.680830, lng: -3.672029},
+      icon : 'img/marker.png'
+    }];
+
+    map = new google.maps.Map(mapDiv, {
+      center: center,
+      zoom: 5.7,
+      styles: [
+  {
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#f5f5f5"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.icon",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#616161"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#f5f5f5"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#bdbdbd"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#eeeeee"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#e5e5e5"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#ffffff"
+      }
+    ]
+  },
+  {
+    "featureType": "road.arterial",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#dadada"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#616161"
+      }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.line",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#e5e5e5"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.station",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#eeeeee"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#c9c9c9"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  }
+]
+    });
+
+  markers.forEach(function(marker){
+    new google.maps.Marker({
+      position: marker.position,
+      map:map,
+      icon: 'img/marker.png'
+      })  
+  })
+}
--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,21 @@
-function initMap() {
-	var map, marker,
-		mapDiv = document.getElementById('map'),
-		center = {
-			lat: 41.920187,
-			lng: 12.497144
-	}
-	var markers = [{
-		position : center,
-		icon : 'img/marker.png'
-		},{
-			position : { lat: 46.052630, lng: 4.845418},
-			icon : 'img/marker.png'
-		},{
-			position : { lat: 40.680830, lng: -3.672029},
-			icon : 'img/marker.png'
-		}];
 
-		map = new google.maps.Map(mapDiv, {
-			center: center,
-			zoom: 5.7 
+$(document).ready(function() {
+		$('.slides-1').slick({
+			dots: false,
+			autoplay: true,
+			autoplaySpeed: 4000,
+			pauseOnFocus: false,
+			pauseOnHover: false,
+			arrows: true,
+			infinite: true
 		});
-
-	markers.forEach(function(marker){
-		new google.maps.Marker({
-			position: marker.position,
-			map:map,
-			icon: 'img/marker.png'
-			})	
-	})
-}
+		$('.slides-2').slick({
+			dots: false,
+			autoplay: true,
+			autoplaySpeed: 4000,
+			pauseOnFocus: false,
+			pauseOnHover: false,
+			arrows: true,
+			infinite: true
+		});
+	});
